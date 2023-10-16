96fd86be17f1e99f58f3d68f95bc6b8cf9c43f45
Archit Kawale <archit.kawale@gmail.com>
2023-10-16 23:48:59 +0530

    Passing Comments

--- inventory/models.py
+++ inventory/models.py
@@ -3,11 +3,13 @@ import uuid
 # Create your models here.
 
 class Product(models.Model):
+    #'id' acts as a tool to connect this model to other models
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, blank=True)
     name = models.CharField(null=False, max_length=256)
     price = models.PositiveIntegerField(null=False)
     hasSub = models.BooleanField(null=False)
     
+    #Makes admin panel look sexy
     def __str__(self):
         return self.name
     
@@ -15,30 +17,38 @@ class Description(models.Model):
     about = models.TextField(null = False)
     dimension = models.CharField(max_length=256)
     weight = models.PositiveIntegerField()
+    # Establishes models connection with Product
+    # One to one connection as each product is expected to have unique description
     product = models.OneToOneField(to=Product, on_delete=models.CASCADE, blank=True, null=True)
-       
+
+    # Makes admin panel look sexy   
     def __str__(self):
         return self.about
     
     
 class SubProduct(Product):
     sub_quality = models.CharField(max_length=256)
+    # Establishes models connection with Product
     product = models.ForeignKey(to = Product, on_delete=models.CASCADE, related_name="subproducts")
 
+    # Makes admin panel look sexy   
     def __str__(self):
         return self.name
     
     
 class Image(models.Model):
     image = models.TextField(default="Test Image")
+    # Establishes models connection with Product
     product = models.ForeignKey(to = Product, on_delete=models.CASCADE)
     
     
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(null=False, max_length=256)
+    # Establishes models connection with Product
     products = models.ForeignKey(to= Product, on_delete= models.DO_NOTHING)
-    
+
+    # Makes admin panel look sexy   
     def __str__(self):
         return self.name
     
