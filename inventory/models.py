cdcb0080e6d1f685ce475fc7eafb620a12ff3b6f
Soham Hegde <sohamhegdessj4@gmail.com>
2023-10-16 21:44:31 +0530

    Changes made

--- inventory/models.py
+++ inventory/models.py
@@ -2,19 +2,24 @@ from django.db import models
 import uuid
 # Create your models here.
 
-
-
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, blank=True)
     name = models.CharField(null=False, max_length=256)
     price = models.PositiveIntegerField(null=False)
     hasSub = models.BooleanField(null=False)
-    description = models.OneToOneField(to=Description,on_delete=models.CASCADE, blank=True, null=True)
     
     def __str__(self):
         return self.name
     
-
+class Description(models.Model):
+    about = models.TextField(null = False)
+    dimension = models.CharField(max_length=256)
+    weight = models.PositiveIntegerField()
+    product = models.OneToOneField(to=Product, on_delete=models.CASCADE, blank=True, null=True)
+       
+    def __str__(self):
+        return self.about
+    
     
 class SubProduct(Product):
     sub_quality = models.CharField(max_length=256)
