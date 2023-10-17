804f2cd9acc413aa699869d9ea6f5cb43af18a80
Soham Hegde <sohamhegdessj4@gmail.com>
2023-10-17 22:25:36 +0530

    initial commmit

--- inventory/views.py
+++ inventory/views.py
@@ -2,11 +2,11 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from .serializer import ProductSerializer, ImageSerializer
-from inventory.models import Product,Image
+from inventory.models import Category, Image, Product
 
 class ProductViewSet(viewsets.ViewSet):
     def list(self, request):
         products = Product.objects.all()
         serialized = ProductSerializer(products, many=True)
         return Response(serialized.data, status=status.HTTP_200_OK)
-    
+
