2322279abfa815e3cce192a840f27045a9a8ef00
Archit Kawale <archit.kawale@gmail.com>
2023-11-30 14:46:39 +0530

    Sexy Admin Board

--- KiaEnterprises/settings.py
+++ KiaEnterprises/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['0.0.0.0','*']
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_soft.apps.AdminSoftDashboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
