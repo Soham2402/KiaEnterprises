a36068ad1af6a8f66dfd679e3e187cc22f2d86d8
Archit Kawale <archit.kawale@gmail.com>
2023-12-12 14:42:19 +0530

    Revert "Lot of Stuff"
    This reverts commit 80572c18ea9fe6792028a0da6b8726c6ab02b59f.
    undid push

--- KiaEnterprises/settings.py
+++ KiaEnterprises/settings.py
@@ -184,10 +184,7 @@ import os
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
--- KiaEnterprises/urls.py
+++ KiaEnterprises/urls.py
@@ -23,6 +23,4 @@ urlpatterns = [
   path('',include(router.urls)),   
   path('auth/',include("authapp.urls"))
   
-]
-urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
