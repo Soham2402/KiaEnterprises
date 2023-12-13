c8d341fbfe5a46b519d65470d6073f26aa333cb2
Soham2402 <sohamhegdessj4@gmail.com>
2023-12-14 00:15:48 +0530

    refactor: added media

--- KiaEnterprises/settings.py
+++ KiaEnterprises/settings.py
@@ -187,8 +187,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')  # Adjusted to include 'media' as a subdirectory
 MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
